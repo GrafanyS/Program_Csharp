d496a6bc35ba4d61c37c46304f0e73aa7ec7dc9b
GrafanyS <admin@valet.of.by>
2023-03-03 14:57:29 +0300

    Presenter -> Ведущий

--- Program/Base/classes/Presenter.cs
+++ Program/Base/classes/Presenter.cs
@@ -1,10 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using GIT_C.Program.Base.AbstractClasses;
+using GIT_C.Program.Base.classes.ui;
+using GIT_C.Program.Base.classes.ParseDate;
 
 namespace GIT_C.Program.Base.classes
 {
+
     /**
      * Объединяет в работу пользовательский интерфейс, парсер строки, проверку на валидность элементов строки
      * Пишет в файлы инфо по заданному формату.
@@ -15,24 +15,38 @@ namespace GIT_C.Program.Base.classes
      */
     public class Presenter
     {
-        public Presenter(){}
+        private CGetData getData;
+        private CDataParseProcessor dataParseProcessor;
+        private ACheckDataProcessor dataProcessor;
+        private AFileWriter fileWriter;
+
+        public Presenter(CGetData getData,
+                     CDataParseProcessor dataParseProcessor,
+                     ACheckDataProcessor dataProcessor,
+                     AFileWriter fileWriter)
+                     {
+                        this.getData = getData;
+                        this.dataParseProcessor = dataParseProcessor;
+                        this.dataProcessor = dataProcessor;
+                        this.fileWriter = fileWriter;
+                     }
 
 
-    //     public void run() : IOException
-    //     {
-    //         dataParseProcessor.parseData(getData.getData());
-    //     //Check quantity
-    //     dataProcessor.checkQuantity(dataParseProcessor.getDataArray());
-    //     //Check full name
-    //     dataProcessor.checkFullName(dataParseProcessor.getFullName());
-    //     //Check birthday
-    //     dataProcessor.checkBirthday(dataParseProcessor.getBirthday());
-    //     //Check sex
-    //     dataProcessor.checkSex(dataParseProcessor.getSex());
-    //     System.out.println("\nAll checks are OK!!!");
-    //     // Write data to file
-    //     fileWriter.writeToFile(dataParseProcessor.getInfoToWrite());
-    // }
+        public void run()
+        {
+            dataParseProcessor.parseData(getData.getData());
+            //Check quantity
+            dataProcessor.checkQuantity(dataParseProcessor.getDataArray());
+            //Check full name
+            dataProcessor.checkFullName(dataParseProcessor.getFullName());
+            //Check birthday
+            dataProcessor.checkBirthday(dataParseProcessor.getBirthday());
+            //Check sex
+            dataProcessor.checkSex(dataParseProcessor.getSex());
+            System.Console.WriteLine("\nAll checks are OK!!!");
+            // Write data to file
+            fileWriter.writeToFile(dataParseProcessor.getInfoToWrite());
+        }
     }
     
 }
