a32abf7d7d2ff1b69fd5307220a72c11d81c0fb7
GrafanyS <admin@valet.of.by>
2023-03-03 10:28:32 +0300

    Полное имя Содержит исключение из цифр

--- Program/Base/Exception/FullNameContainsDigitException.cs
+++ Program/Base/Exception/FullNameContainsDigitException.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 
 namespace GIT_C.Program.Base.Exception
 {
+    // Полное имя Содержит исключение из цифр
     public class FullNameContainsDigitException : FormatException
     {
         public FullNameContainsDigitException(string message)
