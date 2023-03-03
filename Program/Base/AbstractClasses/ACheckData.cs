19150d66680182cdd943c86216433acdfa6be9d0
GrafanyS <admin@valet.of.by>
2023-03-03 12:23:09 +0300

    Абстактный класс проверки данных

--- Program/Base/AbstractClasses/ACheckData.cs
+++ Program/Base/AbstractClasses/ACheckData.cs
@@ -6,8 +6,9 @@ using GIT_C.Program.Base.Interface;
 
 namespace GIT_C.Program.Base.AbstractClasses
 {
+    //  Абстактный класс проверки данных
     public abstract class ACheckData : ICheckElement
     {
-        
+        public abstract bool checkElement(string data);
     }
 }
