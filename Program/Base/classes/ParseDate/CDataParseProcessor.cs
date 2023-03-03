1385990499894d9a9928de22067a3990a67416eb
GrafanyS <admin@valet.of.by>
2023-03-03 15:04:35 +0300

    класс процесс анализа даты

--- Program/Base/classes/ParseDate/CDataParseProcessor.cs
+++ Program/Base/classes/ParseDate/CDataParseProcessor.cs
@@ -7,6 +7,35 @@ namespace GIT_C.Program.Base.classes.ParseDate
 {
     public class CDataParseProcessor
     {
-        
+        // класс процесс анализа даты
+        internal string getBirthday()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal string[] getDataArray()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal string[] getFullName()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal string[] getInfoToWrite()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal string getSex()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void parseData(object value)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
