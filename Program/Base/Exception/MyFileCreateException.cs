f2a2b86aedb11c52d9598b22edf87f6bce755399
GrafanyS <admin@valet.of.by>
2023-03-03 10:39:13 +0300

    Исключение создания моего файла

--- Program/Base/Exception/MyFileCreateException.cs
+++ Program/Base/Exception/MyFileCreateException.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 
 namespace GIT_C.Program.Base.Exception
 {
+    // Исключение создания моего файла
     public class MyFileCreateException : IOException
     {
         public MyFileCreateException(string massage)
