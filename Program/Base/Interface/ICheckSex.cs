1706c1d73a94795abd6007b851280fb3bef47676
GrafanyS <admin@valet.of.by>
2023-03-03 11:48:02 +0300

    Интерфейс проверки пол

--- Program/Base/Interface/ICheckSex.cs
+++ Program/Base/Interface/ICheckSex.cs
@@ -5,8 +5,9 @@ using System.Threading.Tasks;
 
 namespace GIT_C.Program.Base.Interface
 {
+    // Интерфейс проверки пол
     public interface ICheckSex
     {
-        
+        public Boolean checkSex(String sex);
     }
 }
