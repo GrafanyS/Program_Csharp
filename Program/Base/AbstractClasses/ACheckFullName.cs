f0dd618783c2c7ca087c8c35b4ef3f3906248ec0
GrafanyS <admin@valet.of.by>
2023-03-03 11:30:46 +0300

    AbstractClasses

---
+++ Program/Base/AbstractClasses/ACheckFullName.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GIT_C.Program.Base.Interface;
+
+namespace GIT_C.Program.Base.AbstractClasses
+{
+    public abstract class ACheckFullName : ICheckFullName
+    {
+        
+    }
+}
---
+++ Program/Base/AbstractClasses/AFileWriter.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GIT_C.Program.Base.Interface;
+
+namespace GIT_C.Program.Base.AbstractClasses
+{
+    public abstract class AFileWriter : IWriteToFile
+    {
+        public bool writeToFile(string[] infoToWrite)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
