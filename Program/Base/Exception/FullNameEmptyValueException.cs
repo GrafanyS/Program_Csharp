0b784c6fe191ac602f8c67ef1366f128d6a589c2
GrafanyS <admin@valet.of.by>
2023-03-03 10:32:07 +0300

    Полное имя Исключение пустого значения

--- Program/Base/Exception/FullNameEmptyValueException.cs
+++ Program/Base/Exception/FullNameEmptyValueException.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 
 namespace GIT_C.Program.Base.Exception
 {
+    // Полное имя Исключение пустого значения
     public class FullNameEmptyValueException : FormatException
     {
         public FullNameEmptyValueException(string message)
