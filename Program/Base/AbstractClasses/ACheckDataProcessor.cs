5fcf71113219b50a17d644494206c9d69c084a9a
GrafanyS <admin@valet.of.by>
2023-03-03 12:25:47 +0300

    Абстактный класс проверки процесса обработки данных

--- Program/Base/AbstractClasses/ACheckDataProcessor.cs
+++ Program/Base/AbstractClasses/ACheckDataProcessor.cs
@@ -6,8 +6,12 @@ using GIT_C.Program.Base.Interface;
 
 namespace GIT_C.Program.Base.AbstractClasses
 {
+    // Абстактный класс проверки процесса обработки данных
     public abstract class ACheckDataProcessor : ICheckFullName, ICheckQuantity, ICheckBirthday, ICheckSex
     {
-        
+        public abstract bool checkFullName(string[] fullName);
+        public abstract bool checkQuantity(string[] data);
+        public abstract bool checkBirthday(string birthday);
+        public abstract bool checkSex(string sex);
     }
 }
