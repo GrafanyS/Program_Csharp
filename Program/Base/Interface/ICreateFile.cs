35c957b19618ba269998fb31ee7ca534241853cd
GrafanyS <admin@valet.of.by>
2023-03-03 11:58:43 +0300

    интерфейс создать файл

--- Program/Base/Interface/ICreateFile.cs
+++ Program/Base/Interface/ICreateFile.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace GIT_C.Program.Base.Interface
 {
+    // интерфейс создать файл
     public interface ICreateFile
     {
-        
+        public Boolean createFile(String path);
     }
 }
