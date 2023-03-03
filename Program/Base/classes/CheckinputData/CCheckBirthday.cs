c0487c7332fddc129f35452333bc41f03dcd97bc
GrafanyS <admin@valet.of.by>
2023-03-03 11:02:09 +0300

    classes

---
+++ Program/Base/classes/CheckinputData/CCheckBirthday.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GIT_C.Program.Base.classes.CheckinputData
+{
+    public class CCheckBirthday
+    {
+        
+    }
+}
---
+++ Program/Base/classes/Presenter.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GIT_C.Program.Base.classes
+{
+    /**
+     * Объединяет в работу пользовательский интерфейс, парсер строки, проверку на валидность элементов строки
+     * Пишет в файлы инфо по заданному формату.
+     * @param getData - пользовательский интерфейс.
+     * @param dataParseProcessor - парсер строки.
+     * @param dataProcessor - проверка элементов на валидность
+     * @param fileWriter - пишет в файл согласно условию.
+     */
+    public class Presenter
+    {
+        public Presenter(){}
+
+
+    //     public void run() : IOException
+    //     {
+    //         dataParseProcessor.parseData(getData.getData());
+    //     //Check quantity
+    //     dataProcessor.checkQuantity(dataParseProcessor.getDataArray());
+    //     //Check full name
+    //     dataProcessor.checkFullName(dataParseProcessor.getFullName());
+    //     //Check birthday
+    //     dataProcessor.checkBirthday(dataParseProcessor.getBirthday());
+    //     //Check sex
+    //     dataProcessor.checkSex(dataParseProcessor.getSex());
+    //     System.out.println("\nAll checks are OK!!!");
+    //     // Write data to file
+    //     fileWriter.writeToFile(dataParseProcessor.getInfoToWrite());
+    // }
+    }
+    
+}
