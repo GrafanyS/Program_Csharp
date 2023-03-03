2025581d7f79c6baf59dbf8f08e1988293f63365
GrafanyS <admin@valet.of.by>
2023-03-03 15:33:16 +0300

    класс запуска программы

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using GIT_C.Program.Base.classes;
+using GIT_C.Program.Base.classes.CheckinputData;
+using GIT_C.Program.Base.classes.ParseDate;
+using GIT_C.Program.Base.classes.ui;
+using GIT_C.Program.Base.classes.WorkWithFile;
+
+namespace GIT_C.Program
+{
+    //  класс запуска программы
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            // Путь к папке с файлами
+            String infoPathFolder = "Program/data/";
+            Presenter presenter = new Presenter(new CGetData(),
+                                                new CDataParseProcessor(),
+                                                new CCheckDataProcessor(new CCheckQuantity(),
+                                                                        new CCheckFullName(),
+                                                                        new CCheckBirthday(),
+                                                                        new CCheckSex()),
+                                                new CFileWriter(new CFileCreator(),
+                                                                new CFindTheSameFileName(),
+                                                        infoPathFolder));
+            presenter.run();
+        }
+    }
+}
