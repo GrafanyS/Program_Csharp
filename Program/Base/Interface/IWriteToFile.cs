fc3e7baa53c2d0cd6aebeacda5fe9517e640140c
GrafanyS <admin@valet.of.by>
2023-03-03 12:17:15 +0300

    интерфейс записи в файл

--- Program/Base/Interface/IWriteToFile.cs
+++ Program/Base/Interface/IWriteToFile.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace GIT_C.Program.Base.Interface
 {
+    // интерфейс записи в файл
     using GIT_C.Program.Base.Exception;
     public interface IWriteToFile
     {
