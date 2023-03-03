978e0b82445d060c514c9f9e7ec29874a626180a
GrafanyS <admin@valet.of.by>
2023-03-03 10:22:37 +0300

    Исключение из проверки количества

--- Program/Base/Exception/CheckQuantityException.cs
+++ Program/Base/Exception/CheckQuantityException.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 
 namespace GIT_C.Program.Base.Exception
 {
+    // Исключение из проверки количества
     public class CheckQuantityException : IOException
     {
         public CheckQuantityException(string message)
