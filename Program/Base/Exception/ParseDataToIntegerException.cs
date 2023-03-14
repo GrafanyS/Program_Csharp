81ba29427d83c13fb281cf430b1c1358b67267dc
GrafanyS <admin@valet.of.by>
2023-03-14 09:08:41 +0300

    Преобразовать Данные в Целочисленное Исключение

--- Program/Base/Exception/ParseDataToIntegerException.cs
+++ Program/Base/Exception/ParseDataToIntegerException.cs
@@ -8,6 +8,7 @@ namespace GIT_C.Program.Base.Exception
     // Преобразовать Данные в Целочисленное Исключение
     public class ParseDataToIntegerException : IOException
     {
+        public ParseDataToIntegerException(){}
         public ParseDataToIntegerException(string message)
             : base(message)
         {}
